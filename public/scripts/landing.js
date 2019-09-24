1b2b0509d73137345102f9f5317890841f8c5114
B-Musick <bmuze1@gmail.com>
2019-09-23 20:16:32 -0600

    All tabs work, all main pages styled, need to include blog and contact info

--- public/scripts/landing.js
+++ public/scripts/landing.js
@@ -3,14 +3,17 @@ let allButtons = document.querySelector('.landing-foldable'); // Select all butt
 let headshot = document.querySelector('.mission-statement-headshot');
 
 // BUTTON CONTAINERS
+let brendanButton = document.getElementById('landing-brendan');
 let aboutButton = document.querySelector('#landing-about');
 let missionButton = document.getElementById('landing-mission'); // Mission button
 let technologiesButton = document.getElementById('landing-technologies'); // Tech button
 let learningButton = document.getElementById('landing-learning'); // Learning button
+let projectsButton= document.getElementById('landing-projects');
 
-let buttonArray = [aboutButton,missionButton,technologiesButton,learningButton];
+let buttonArray = [brendanButton,aboutButton,missionButton,technologiesButton,learningButton, projectsButton];
 
 // BUTTON TEXT
+let brendanDescriptionContainer = document.getElementById('brendan-description-container');
 let aboutText = document.getElementById("landing-about-text");
 let missionText = document.getElementById("landing-mission-text");
 let technologiesText = document.getElementById("landing-technologies-text");
@@ -18,10 +21,11 @@ let learningText = document.getElementById("landing-learning-text");
 
 let textArray = [aboutText,missionText,technologiesText,learningText];
 
-// DESCRIPTION CONTAINER
-let about = document.getElementById("about-description-container");
+
 
 let lastTextShown; // Use to take away the text from the last button
+
+
 
 buttonArray.forEach(button=>{
     /* Go through each button and when clicked, transistion that button to the top,
@@ -50,16 +54,17 @@ buttonArray.forEach(button=>{
                  
                 
             }else{
+                // Transition the button to the top of the page
                 button.classList.add('landing-button-transition');
                 button.classList.remove('foldable-retreat')
+
                 // Get the tab name and set this as description container to use
                 // id is named 'landing-tabName'
                 let split = button.id.split('-');
                 let containerName = split[1];
-
                 let container = document.getElementById(containerName+'-description-container');
                 
-                
+                // Move the description to show on the screen
                 container.classList.remove('description-container'); 
                 container.classList.add('description-inside-screen');
                 
@@ -81,6 +86,32 @@ document.addEventListener('click',e=>{
         // If pressed the page, then return to ground zero
         buttonArray.forEach(button => button.classList.remove('landing-button-transition'))
         buttonArray.forEach(button=>{button.classList.remove('foldable-retreat')});
+
+        // REMOVE TEXT FROM SCREEN
+        buttonArray.forEach(button => {
+        
+            let split = button.id.split('-');
+            let containerName = split[1];
+
+            // Get the container holding the text of the button not pressed
+            let container = document.getElementById(containerName + '-description-container');
+
+            // Make it so the text doesnt show for the other containers not pressed
+            container.classList.add('description-container');
+            container.classList.remove('description-inside-screen'); 
+        })
+
+        // REPLACE THE BRENDAN MUSICK SCREEN TO THE FOREFRONT
+        buttonArray[0].classList.add('landing-button-transition');
+        let split = buttonArray[0].id.split('-');
+        let containerName = split[1];
+
+        // Get the container holding the text of the button not pressed
+        let container = document.getElementById(containerName + '-description-container');
+        
+        // Make it so the text doesnt show for the other containers not pressed
+        container.classList.remove('description-container');
+        container.classList.add('description-inside-screen'); 
     }
 })
 
